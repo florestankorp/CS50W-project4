d7b05750b079415d381cd8b2886458fb19749b10
Florestan Korp <florestankorp@gmail.com>
2020-09-04 19:55:00 +0200

    simple navigation works

--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django_seed import Seed
 
-# Create your tests here.
+from network.models import Post, User
+
+seeder = Seed.seeder()
+
+seeder.add_entity(User, 10, {"email": seeder.faker.email()})
+seeder.add_entity(Post, 5)
+
+inserted_pks = seeder.execute()
